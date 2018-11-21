7cd0183abde2272fc1210b6b23f08e72f1363ba0
kihoon <koreakihoon@gmail.com>
2018-11-21 14:25:17 +0900

    create Home

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="app">
+    <Home/>
   </div>
 </template>
 
 <script>
+import Home from './views/Home';
+
+export default {
+  name: 'app',
+  components: {
+    Home
+  }
+}
 </script>
 
 <style lang="scss">
