1d78277e1dd81666f20cda422ee48bfc48a64f16
Kihoon Kim <koreakihoon@gmail.com>
2018-11-21 22:58:25 +0900

    add moment and apply D-days

--- src/components/MovieCardList.vue
+++ src/components/MovieCardList.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
     <h2>{{titleName}}</h2>
-    <div v-for="movie in movies"
+    <div class="movie-card-list"
+      v-for="movie in movies"
       :key="movie.id">
       <MovieImage v-bind:img-url="movie.thumb" 
         v-bind:movie-ratings="movie.movieRatings"/>
       <div>{{movie.name}}</div>
-      <div>{{movie.releaseDate}}</div>
+      <div>{{formatedReleaseDate(movie.releaseDate)}}</div>
     </div>
   </div>
 </template>
 
 <script>
 import MovieImage from '@/components/MovieImage.vue';
+import moment from 'moment';
 
 export default {
   props: {
@@ -27,10 +29,23 @@ export default {
   },
   components: {
     MovieImage,
+  },
+  methods: {
+    formatedReleaseDate(releaseDate) {
+      const today = moment();
+
+      const diffDays = moment(releaseDate).diff(today, 'days');
+      
+      return diffDays > 0 ? `D-${diffDays}` : releaseDate;
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.movie-card-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 </style>
